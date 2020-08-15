051196e97d843d03f42ba788f61bb0dee75630e8
elifyurt <yrtelf@gmail.com>
2020-08-15 22:37:51 +0300

    combine and return lambdas

--- src/LambdasAndHighOrderFunctions/HighOrderFunctions.kt
+++ src/LambdasAndHighOrderFunctions/HighOrderFunctions.kt
@@ -2,22 +2,45 @@ package LambdasAndHighOrderFunctions
 
 fun main() {
 
-    convert(20.0) { it * 1.8 + 32 }
-    convertFive { it * 1.8 + 32 }
+    //convert 2.5 kg to pounds
+    println("Convert 2.5 kg to pounds: ${getConversionLambda("KgsToPounds")(2.5)}")
+
+    //define two conversion lambdas
+    val kgsToPoundsLambda = getConversionLambda("KgsToPounds")
+    val poundsToUSTonsLambda = getConversionLambda("PoundsToUSTons")
+
+    //combine the two lambdas to create a new one
+    val kgsToUSTonsLambda = combine(kgsToPoundsLambda, poundsToUSTonsLambda)
+
+    //use the new lambda to convert 17.4 to US tons
+    val value = 17.4
+    println("$value kgs is ${convert(value, kgsToUSTonsLambda)} US tons")
 
 }
 
 /**
  * Celcius to Fahrenheit
  */
-fun convert(x: Double, converter: (Double) -> Double): Double {
+fun convert(x: Double, converter: DoubleConversion): Double {
     val result = converter(x)
     println("$x is converted to $result")
     return result
 }
 
-fun convertFive(converter: (Int) -> Double): Double {
-    val result = converter(5)
-    println("5 is converted to $result")
-    return result
+typealias DoubleConversion = (Double) -> Double
+
+fun getConversionLambda(str: String): DoubleConversion {
+    if (str == "CentigradeToFahrenheit") {
+        return { it * 1.8 + 32 }
+    } else if (str == "KgsToPounds") {
+        return { it * 2.204623 }
+    } else if (str == "PoundsToUSTons") {
+        return { it / 2000.0 }
+    } else {
+        return { it }
+    }
+}
+
+fun combine(lambda1: DoubleConversion, lambda2: DoubleConversion): DoubleConversion {
+    return { x: Double -> lambda2(lambda1(x)) }
 }
