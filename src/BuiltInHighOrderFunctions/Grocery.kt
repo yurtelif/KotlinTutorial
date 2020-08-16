4ba7b8f119a6a34e954e9d7b94d4d2abe3e72c01
elifyurt <yrtelf@gmail.com>
2020-08-16 15:49:06 +0300

    filter and map implementation added

--- src/BuiltInHighOrderFunctions/Grocery.kt
+++ src/BuiltInHighOrderFunctions/Grocery.kt
@@ -22,4 +22,30 @@ fun main() {
     println("sumQuantity: $sumQuantity")
     val totalPrice = groceries.sumByDouble { it.quantity * it.unitPrice }
     println("totalPrice: $totalPrice")
+
+    val vegetables = groceries.filter { it.category == "Vegetable" }
+    println("Vegetables: $vegetables")
+    val notFrozen = groceries.filterNot { it.category != "Frozen" }
+    println("notFrozen: $notFrozen")
+
+    val groceryNames = groceries.map { it.name }
+    println("groceryNames: $groceryNames")
+    val halfUnitPrice = groceries.map { it.unitPrice * 0.5 }
+    println("halfUnitPrice: $halfUnitPrice")
+
+    val newPrices = groceries.filter { it.unitPrice > 3.0 }
+            .map { it.unitPrice * 2 }
+    println("newPrices: $newPrices")
+
+    println("Grocery names: ")
+    groceries.forEach { println(it.name) }
+
+    println("Groceries with unitPrice > 3.0: ")
+    groceries.filter { it.unitPrice > 3.0 }
+            .forEach { println(it.name) }
+
+    var iteNames = ""
+    groceries.forEach { iteNames += "${it.name} " }
+    println("itemNames: $iteNames")
+
 }
